8c594024f52a966fc68235ae4065f61e5df747ec
Nicolas Hammond <nhammond@hammondsoftware.com>
2014-10-18 11:02:04 -0400

    Wiki cheats.

---
+++ wget/get_all.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Script to grab data from Wikipedia.
+# Used it to grab WBF IDs
+mkdir -p players
+cat names.txt | while read file
+do
+  echo "Doing $file"
+	wget -O "players/$file" "http://en.wikipedia.org/wiki/$file"  >"players/${file}.out" 2>"players/$file.err"
+done
+
