749af65130789deeedce81932751201f7295098d
Nicolas Hammond <nhammond@hammondsoftware.com>
2014-10-19 16:58:25 -0400

    Update results. Move alternative names/misspellings found.

--- move_players_to_results.sh
+++ move_players_to_results.sh
@@ -15,7 +15,7 @@ echo "Ignore any errors about no player starting with Q, X, Y"
 for dir in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 do
 #  if [ -e ${dir}* ]; then
-    mv ${dir}* ../results/${dir}
+    cp ${dir}* ../results/${dir}
 #	fi
 done
 
