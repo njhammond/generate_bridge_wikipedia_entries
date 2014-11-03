8fdd1c0ca425399c7110ea8e978459620833f719
Nicolas Hammond <nhammond@hammondsoftware.com>
2014-11-03 15:04:16 -0500

    Added some more alternative names

--- move_players_to_results.sh
+++ move_players_to_results.sh
@@ -11,7 +11,7 @@ done
 cd ..
 cd players
 
-echo "Ignore any errors about no player starting with Q, X, Y"
+echo "Ignore any errors about no player starting with U"
 for dir in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 do
 #  if [ -e ${dir}* ]; then
